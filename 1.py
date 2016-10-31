548e8b0b5941122f91db6f59662b0609c7531079
jdfandino10 <jd.fandino10@uniandes.edu.co>
2016-10-30 23:58:52 -0500

    Agregar espacios 1.py

--- 1.py
+++ 1.py
@@ -1,8 +1,8 @@
 from numpy import *
 
 #s="g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj"
-s="map.html"
-ss=""
+s = "map.html"
+ss = ""
 
 abc = array(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
 
@@ -11,7 +11,7 @@ for i in range(len(s)):
     if(p == " " or p == "." or p == "(" or p == ")" or p == "'" or p == "/" or p == ":"):
         ss+=p
     else:    
-        ind = where(abc==p)[0][0]
+        ind = where(abc == p)[0][0]
         if(ind<24):
             ss += abc[ind+2]
         elif(ind==24):
